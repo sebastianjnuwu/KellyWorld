44ecc1b994d5d848664201de98832f34c98d0499
SEBASTIAN JN ฅ^•ﻌ•^ฅ <101356556+sebastianjn@users.noreply.github.com>
2022-03-15 20:48:29 +0000

    Começando o bot....

--- src/database/pt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    ola : "<:K_env:938833579981566043> Olá meu nome é Kelly World me chamou?",
-	}
--- src/lingua.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	pt : require('../src/database/pt.js'),
-	en : require('../src/database/en.js'),
-	es : require('../src/database/es.js')
-};
