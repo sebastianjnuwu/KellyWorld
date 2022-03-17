5778d9cf81844014bfd3338fbd73daa74f626364
SEBASTIAN JN ฅ^•ﻌ•^ฅ <101356556+sebastianjn@users.noreply.github.com>
2022-03-17 12:17:02 -0300

    update ip.js

--- src/commands/KettraWorld/ip.js
+++ src/commands/KettraWorld/ip.js
@@ -9,7 +9,12 @@ try {
                 
   message.author.send(`**Java Edition: \`kettraworld.jogar.io\`\nBedrock: \`190.115.197.81\`\nPorta: \`10001\`**`);
 
-  message.reply("olhe o seu pv querido! ^^")
+  message.reply("olhe o seu pv querido! ^^").then(m => {
+                setTimeout(() => {
+                    m.delete()
+            
+                }, 60000) 
+            })
  
         } catch (e) {
             message.reply(`**Java Edition: \`kettraworld.jogar.io\`\nBedrock: \`190.115.197.81\`\nPorta: \`10001\`**`).then(m => {
