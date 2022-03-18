b053e4501bf90b1bdf744bd48f349a88c15d00b5
SEBASTIAN JN ฅ^•ﻌ•^ฅ <101356556+sebastianjn@users.noreply.github.com>
2022-03-18 13:08:11 -0300

    update clear.js

--- src/commands/admin /clear.js
+++ src/commands/admin /clear.js
@@ -8,7 +8,10 @@ module.exports = {
     run: async (client, message, args, Discord) => {
         try {
             let delamount = args[0];
-
+         
+           if(!message.guild.me.permissions.has("ADMINISTRATOR")) {
+    return message.reply("<:K_zan:924366252024164363>  eu tou sem ah permissão de `ADMINISTRADOR` infelizmente sou inútil ಥ╭╮ಥ")
+  }
             if (!message.member.permissions.has("MANAGE_MESSAGES")){return message.channel.send(`Lhe falta permissão para executar esse comando.`)}
 
             if (isNaN(delamount) || parseInt(delamount <= 0)) return message.reply('**Você não colocou nenhum número.**')
