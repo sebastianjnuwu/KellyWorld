6639a54feabb26f4c70c1ed0a2a1ca8329b16768
SEBASTIAN JN ฅ^•ﻌ•^ฅ <101356556+sebastianjn@users.noreply.github.com>
2022-03-18 13:10:12 -0300

    update avatar.js

--- src/commands/geral/avatar.js
+++ src/commands/geral/avatar.js
@@ -4,6 +4,11 @@ module.exports = {
   name: "avatar",
   aliases: ['avatar', 'avata'],
   async run(bot, message, args) {
+  
+  if(!message.guild.me.permissions.has("ADMINISTRATOR")) {
+    return message.reply("<:K_zan:924366252024164363>  eu tou sem ah permissão de `ADMINISTRADOR` infelizmente sou inútil ಥ╭╮ಥ")
+  }
+  
   let user = message.mentions.users.first() || bot.users.cache.get(args[0]) || message.author;
   const EMBED = new Discord.MessageEmbed()
     .setTitle(`🖼 ${user.username}`)
