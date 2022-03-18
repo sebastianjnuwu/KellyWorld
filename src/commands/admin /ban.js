0c6eb84317d9f8c02f62f490715608d29b62e52b
SEBASTIAN JN ฅ^•ﻌ•^ฅ <101356556+sebastianjn@users.noreply.github.com>
2022-03-18 20:57:22 -0300

    update ban.js

---
+++ src/commands/admin /ban.js
@@ -0,0 +1,112 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+  name: 'ban',
+  run: async (client, message, args) => {
+
+if(!message.guild.me.permissions.has("ADMINISTRATOR")) {     return message.reply("<:K_zan:924366252024164363>  eu tou sem ah permissão de `ADMINISTRADOR` infelizmente sou inútil ಥ╭╮ಥ") 
+}
+    if(!message.member.permissions.has('BAN_MEMBERS')) return message.reply(`${message.author}\n**Você não possui a permissão para banir membros.**`).then(mDel => {
+      setTimeout(() => {
+        mDel.delete().catch(o_U => {})
+        message.delete().catch(o_U => {})
+      }, 10000)
+    })
+
+    if(!message.guild.me.permissions.has('BAN_MEMBERS')) return message.reply(`${message.author}\n**Eu não tenho permissão para banir membros.**`).then(mDel => {
+      setTimeout(() => {
+        mDel.delete().catch(o_U => {})
+        message.delete().catch(o_U => {})
+      }, 10000)
+    })
+
+    if(!args[0]) return message.reply(`${message.author}\n**Você não informou o membro a ser banido.**`).then(mDel => {
+      setTimeout(() => {
+        mDel.delete().catch(o_U => {})
+        message.delete().catch(o_U => {})
+      }, 10000)
+    })
+
+    if(isNaN(args[0]) && !args[0].startsWith('<@') && !args[0].endsWith('>')) return message.reply(`${message.author}\n**Isto não é uma menção ou um id válido.**`).then(mDel => {
+      setTimeout(() => {
+        mDel.delete().catch(o_U => {})
+        message.delete().catch(o_U => {})
+      }, 10000)
+    })
+
+    let member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
+
+    if(!member) return message.reply(`Este membro não existe neste servidor.`).then(mDel => {
+      setTimeout(() => {
+        mDel.delete().catch(o_U => {})
+        message.delete().catch(o_U => {})
+      }, 10000)
+    })
+    if(member.id === message.author.id) return message.reply(`${message.author}\n**Você não pode banir a si mesmo.**`).then(mDel => {
+      setTimeout(() => {
+        mDel.delete().catch(o_U => {})
+        message.delete().catch(o_U => {})
+      }, 10000)
+    })
+    if(member.id === this.client.user.id) return message.reply(`${message.author}\n**Você não pode me banir usando o meu comando.**`).then(mDel => {
+      setTimeout(() => {
+        mDel.delete().catch(o_U => {})
+        message.delete().catch(o_U => {})
+      }, 10000)
+    })
+    if(!member.bannable) return message.reply(`${message.author}\n**Eu não consigo banir este membro.**`).then(mDel => {
+      setTimeout(() => {
+        mDel.delete().catch(o_U => {})
+        message.delete().catch(o_U => {})
+      }, 10000)
+    })
+
+    let reason = !args.slice(1).join(' ') ? 'Não Informado' : args.slice(1).join(' ')
+    
+    message.reply(`${message.author}\n**Você realmente desaja banir o membro** ${member}?\n*Caso deseja cancelar o banimento clique na reação ❌.*\n*Caso queira banir o membro clique na reação ✅.*`).then(async (msg) => {
+      msg.react('✅')
+      msg.react('❌')
+
+      const filter = (reaction, user) => user.id === message.author.id
+
+      const collector = msg.createReactionCollector({ filter, max: 1, time: 60000 });
+
+      collector.on('end', reaction => {
+        let emoji = reaction.map(x => x.emoji.name).toString()
+
+        if(emoji === '✅') {
+          let embed = new MessageEmbed()
+          .setTitle('🛑 VOCÊ FOI BANIDO')
+          .setColor('RED')
+          .setDescription(`**Você foi banido no servidor \`${message.guild.name}\`.**`)
+          .addFields(
+            { name: '📌 Motivo', value: reason }
+          )
+          .setFooter(`Banido por ${message.author.username}`)
+          member.send({ embeds: [embed] }).catch(o_U => {})
+          msg.edit(`${message.author}\n**O membro \`${member.user.tag}\` foi banido do servidor com sucesso.**\n*Motivo: **${reason}***`).then(mDel => {
+            setTimeout(() => {
+              mDel.delete().catch(o_U => {})
+              message.delete().catch(o_U => {})
+            }, 10000)
+          })
+          member.ban({ reason: reason })
+        } else if(emoji === '❌') {
+          msg.edit(`${message.author}\n**O banimento de ${member} foi cancelado com sucesso.**`).then(mDel => {
+            setTimeout(() => {
+              mDel.delete().catch(o_U => {})
+              message.delete().catch(o_U => {})
+            }, 10000)
+          })
+        }
+      })
+
+      setTimeout(() => {
+        msg.delete().catch(o_U => {})
+        message.delete().catch(o_U => {})
+      }, 60000)
+
+    })
+
+  }
+}
