0c1398dfdb80cf899822578a5f012ee24ac2c214
SEBASTIAN JN ฅ^•ﻌ•^ฅ <101356556+sebastianjn@users.noreply.github.com>
2022-03-20 10:30:48 -0300

    update ping.js

--- src/commands/geral/ping.js
+++ src/commands/geral/ping.js
@@ -11,16 +11,16 @@ module.exports = {
   }
   
   const embed = new Discord.MessageEmbed()
-  .setDescription("**Ping sendo Calculado...**")
-  .setColor(`RANDOM`)
+  .setDescription("**Ping sendo Calculado...**");
+  .setColor(`RANDOM`);
   message.channel.send({ embeds: [embed]}).then(msg => {
     setTimeout(() => {
       let embed2 = new Discord.MessageEmbed()
       .setTitle(`🏓 Pong!`)
       .setDescription(`Latência da API: **${Math.round(client.ws.ping)}ms**\nLatência do Servidor: **${msg.createdTimestamp - message.createdTimestamp}ms**`)
       .setColor(`RANDOM`)
-    msg.edit({embeds: [embed2]})
+    msg.edit({embeds: [embed2]});
     }, 5000)
   });
 }
-}
+};
