cb1d0f0d320a0cf274d6f457fd682f7810272a57
SEBASTIAN JN ฅ^•ﻌ•^ฅ <101356556+sebastianjn@users.noreply.github.com>
2022-03-21 14:41:04 -0300

    update kettra.js

--- src/commands/kettra/kettra.js
+++ src/commands/kettra/kettra.js
@@ -7,7 +7,6 @@ module.exports = {
 run: async(client, message, args) => {
  
 	if( message.guild.id !== "893997835412971570") {
-		
 	  return;
 		
 	} else {
@@ -28,7 +27,25 @@ run: async(client, message, args) => {
     } else if (args[0] == 'voto') {
       
         message.reply("vocé ja deu 1 voto para kettra? clique aqui: https://kettraworld.github.io/votar.html");
-
+         
+    } else if (args[0] == 'historia') {
+      const content = args.join(" ");
+      var canal = message.guild.channels.cache.find(ch => ch.id === "955517600236986389");
+      
+      const message_msg = new MessageEmbed()
+    .setColor("RANDOM")
+    .setTitle(`:smile_cat: Temos uma nova pergunta!`)
+    .setThumbnail(message.author.displayAvatarURL())
+    .setDescription(`Enviado por ${message.author}\n\n**pergunta:** ${content}\nㅤ`)
+    .setTimestamp()    
+    .setFooter({ text:`Obrigado ${message.author.tag} por qierwr conhecer mais sobre kettra...`})
+    
+    message.channel.send(`${message.author} a mensagem foi enviada com sucesso!`).then(k => {
+	setTimeout(() => {
+                    k.delete()
+               
+                }, 12000) 
+            })
     } 
   }
 }
