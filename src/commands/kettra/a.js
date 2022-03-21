e11fa79e20657fe70a21b1a791e1885c43445c97
SEBASTIAN JN ฅ^•ﻌ•^ฅ <101356556+sebastianjn@users.noreply.github.com>
2022-03-21 15:28:56 -0300

    update a.js

---
+++ src/commands/kettra/a.js
@@ -0,0 +1,28 @@
+module.exports = {
+    name: "a",
+    aliases: ["a"],
+
+    run: async(client, message, args) => {
+
+        if (!args[0]) return message.reply(`Escreva uma pergunta.`);
+        let respostas = ["sim", "provavelmente sim", "provavelmente não", "não", "óbvio que não", "Não faço ideia", "Não pergunte para mim", "Pergunta isso pro dono da padaria", "Nunca nem vi"];
+        let resposta = respostas[Math.floor(Math.random()*respostas.length)];
+
+        try {
+
+        message.channel.createWebhook('Vieirinha', {
+            avatar: `${message.author.displayAvatarURL()}`,
+
+        }).then(web => {
+            web.send(`${message.author} ${resposta}`)
+            .then(()=> {web.delete() })
+        })
+
+
+    } catch (e) { console.log(e); message.reply(`Eu estou sem a permissão de criar webhooks.`) }
+        
+
+       
+        
+    }
+          }
