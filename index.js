9f604649c1a5982dfe09a9dee413bfdf7c90363b
SEBASTIAN JN ฅ^•ﻌ•^ฅ <101356556+sebastianjn@users.noreply.github.com>
2022-04-03 11:12:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+var http = require('http');
 
+http.createServer(function (req, res) {
+
+res.writeHead(200);
+
+res.end("{'token':'S2V0dHJhd29ybGQ='}");	
+	const ping = new Date();
+
+  ping.setHours(ping.getHours() - 3);	
+
+       console.log(`[Info] Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`);
+
+}).listen(process.env.PORT); // com certeza é a 8080
 
     process.on('unhandledRejection', (reason, p) => {    
        console.log(' [ ANTICLASH ] | SCRIPT REJEITADO');    
