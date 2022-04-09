6527ad5c123e46520fc4061e1ad4c994336d2174
SEBASTIAN JN ฅ^•ﻌ•^ฅ <101356556+sebastianjn@users.noreply.github.com>
2022-04-09 07:27:50 -0300

    update index.js

--- src/handler/index.js
+++ src/handler/index.js
@@ -1,4 +1,5 @@
 const { glob } = require("glob");
+const { fs } = require("kettraworld.db");
 const { promisify } = require("util");
 const { Client } = require("discord.js");
 const globPromise = promisify(glob);
@@ -28,3 +29,15 @@ module.exports = async (client) => {
     );
 
 };
+
+fs.readdirSync('../../src/commands/').forEach(local => {
+    const comandos = fs.readdirSync(`../../src/commands/${local}`).filter(arquivo => arquivo.endsWith('.js'))
+    for(let file of comandos) {
+        let puxar= require(`../../src/commands/${local}/${file}`)
+        if(puxar.name) {
+            client.commands.set(puxar.name, puxar)
+        } 
+        if(puxar.aliases && Array.isArray(puxar.aliases))
+        puxar.aliases.forEach(x => client.aliases.set(x, puxar.name))
+    }
+});
