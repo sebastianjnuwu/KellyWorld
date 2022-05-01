ad40748b5c47a26f29564209cb1224487b5c2ab9
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-05-01 08:54:50 -0300

    update help.js

--- src/commands/commad/help.js
+++ src/commands/commad/help.js
@@ -23,7 +23,7 @@ module.exports = {
       .setColor("RANDOM")
       .setTitle('🔍 commandos!')
 	  .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
-      .setDescription('"Esta menina tem dois lados um é o sol o outro é a lua. Com o sol ela expressa felicidade e alegria. Com a lua ele expressa seu medo e tristeza."\n\n')
+      .setDescription('"Esta menina tem dois lados um é o sol o outro é a lua. Com o sol ela expressa felicidade e alegria. Com a lua ele expressa seu medo e tristeza."\n\nUma garota de 17 anos que busca a ajudar o próximo e a humanidade, com a sua disciplina ela ajudar manter seu seu servidor mais organizado e seguro para que os membros tenha um ambiente agradável!\n\n<:K_env:938833579981566043> Veja meus comandos clicando [aqui]()')
       message.reply({ embeds: [help] })
  };
 	  
