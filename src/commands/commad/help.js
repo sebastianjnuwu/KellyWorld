67e367735bca57131a51e0ce4778784dec3a7653
SEBASTIAN JN ฅ^•ﻌ•^ฅ <101356556+sebastianjnuwu@users.noreply.github.com>
2022-05-01 11:15:41 +0000

    Help??????

---
+++ src/commands/commad/help.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const { JsonDatabase } = require('kettraworld.db');
+const db = new JsonDatabase({
+  DatabaseJson:"./src/database/database.json"
+});
+
+module.exports = {
+    name: "ajuda!",
+    aliases: ["help","ajuda","ayudar"],
+
+  run: async(client, message, args) => {
+
+ let language = db.get(`language_${message.guild.id}`);
+ 
+ if( language == null ) { 
+     db.set(`language_${messag.guild.id}`, "pt");
+}
+
+ if (language === "pt") {
+            if(!message.guild.me.permissions.has("ADMINISTRATOR")) {
+  return message.reply("<:K_zan:924366252024164363> eu tou sem ah permissão de `ADMINISTRADOR` infelizmente sou inútil ಥ╭╮ಥ")
+  };
