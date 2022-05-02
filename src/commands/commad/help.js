d57fd1db45b5b865c417df0faf64a4054e5dc4f3
SEBASTIAN JN ฅ^•ﻌ•^ฅ <101356556+sebastianjnuwu@users.noreply.github.com>
2022-05-02 01:04:54 +0000

    Up

--- src/commands/commad/help.js
+++ src/commands/commad/help.js
@@ -23,7 +23,7 @@ module.exports = {
       .setColor("RANDOM")
       .setTitle('🔍 commandos!')
 	  .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
-      .setDescription('"Esta menina tem dois lados um é o sol o outro é a lua. Com o sol ela expressa felicidade e alegria. Com a lua ele expressa seu medo e tristeza."\n\nUma garota de 17 anos que busca a ajudar o próximo e a humanidade, com a sua disciplina ela ajudar manter seu seu servidor mais organizado e seguro para que os membros tenha um ambiente agradável!\n\n<:K_env:938833579981566043> Veja meus comandos clicando [aqui](https://KellyWorld.kettraworld.repl.co)\n\n<:K_morri:927681449929359410> Encontrou algum bug? click [aqui](https://github.com/sebastianjnuwu/KellyWorld/issues)\n<:K_co:924360190986432522>  Me ajude a ficar online! click [aqui](https://KellyWorld.kettraworld.repl.co)')
+      .setDescription('"Esta menina tem dois lados um é o sol o outro é a lua. Com o sol ela expressa felicidade e alegria. Com a lua ele expressa seu medo e tristeza."\n\nUma garota de 17 anos que busca a ajudar o próximo e a humanidade, com a sua disciplina ela ajudar manter seu seu servidor mais organizado e seguro para que os membros tenha um ambiente agradável!\n\n<:K_env:938833579981566043> Veja meus comandos clicando [aqui](https://KellyWorld.kettraworld.repl.co)\n\n<:K_morri:927681449929359410> Encontrou algum bug? click [aqui](https://github.com/sebastianjnuwu/KellyWorld/issues)\n\n<:K_co:924360190986432522>  Me ajude a ficar online! click [aqui](https://KellyWorld.kettraworld.repl.co)')
       message.reply({ embeds: [help] })
  };
 	  
