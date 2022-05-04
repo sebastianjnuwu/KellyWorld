65c6ec7532649e207296fdba97aba9a8e382f8a5
SEBASTIAN JN ฅ^•ﻌ•^ฅ <101356556+sebastianjnuwu@users.noreply.github.com>
2022-05-04 12:43:17 +0000

    A

---
+++ src/commands/commad/meme.js
@@ -0,0 +1,40 @@
+earconst Discord = require("discord.js");
+por{sonDatabase } = require('kettraworld.db');const db = new JsonDatabase({
+  DatabaseJson:"./src/database/database.json"
+});
+
+module.exports = {
+    name: "meme",
+    aliases: ["meme"],
+
+	run: async(client, message, args) => {
+    
+        let language = db.get(`language_${message.guild.id}`);
+    if( language == null ) { 
+      db.set(`language_${messag.guild.id}`, "pt");
+    }
+    
+    if (language === "pt") {
+        if(!message.guild.me.permissions.has("ADMINISTRATOR")) {
+  return message.reply("<:K_zan:924366252024164363> eu tou sem ah permissão de `ADMINISTRADOR` infelizmente sou inútil ಥ╭╮ಥ")
+  };
+  
+      let ping = new Discord.MessageEmbed()
+	  got('https://www.reddit.com/r/memes/random/.json').then(response => {
+			const [list] = JSON.parse(response.body);
+			const [post] = list.data.children;
+		  const permalink = post.data.permalink;
+			const memeUrl = `https://reddit.com${permalink}`;
+			const memeImage = post.data.url;
+			const memeTitle = post.data.title;
+
+		   ping.setTitle(`${memeTitle}`);
+		ping.setColor('RANDOM');
+		ping.setImage(memeImage);
+		message.reply({ embeds: [ping] })
+    })
+		.catch(console.error);
+
+	}
+	}
+}
