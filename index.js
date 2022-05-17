65b9264b03864fc4328cf32c34c685dbe17121c4
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-05-17 13:35:38 -0300

    update index.js

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ client.on("messageCreate", message => {
     if (message.author.bot) return;
     if (message.channel.type == "") return
     if (message.content == `<@${client.user.id}>` || message.content == `<@!${client.user.id}>`) {
-      const mgs = ['<:K_Magnifico:963485603343761428> Olaa estava dormindo sonhando em ser.........','','','','','','','','','','',''];
+      const mgs = ['<:K_Magnifico:963485603343761428> Olaa estava dormindo sonhando em ser.........','<:K_Belissimo:963485576244375572> Olá como eu posso te ajudar? Só lamento ter acabado o café!','','','','','','','','','',''];
       let mes = () => mgs[~~(Math.random() * mgs.length)];
       
      message.reply(`${mes()}`);
