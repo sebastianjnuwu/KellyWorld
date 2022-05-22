4f5e44cef0a8b6750044ca16dd03c59013ab9975
SEBASTIAN JN ฅ^•ﻌ•^ฅ <101356556+sebastianjnuwu@users.noreply.github.com>
2022-05-22 14:23:18 +0000

    Update

--- index.js
+++ index.js
@@ -55,17 +55,17 @@ client.aliases = new Discord.Collection();
 module.exports = client;
 client.slashCommands = new Discord.Collection();
 require("./src/handler")(client);
-client.categories = fs.readdirSync(`./src/commands/`);
+client.categories = fs.readdirSync("./src/commands/");
 fs.readdirSync('./src/commands/').forEach(local => {
-    const comandos = fs.readdirSync(`./src/commands/${local}`).filter(arquivo => arquivo.endsWith('.js'))
-    for(let file of comandos) {
-        let puxar= require(`./src/commands/${local}/${file}`)
-        if(puxar.name) {
-            client.commands.set(puxar.name, puxar)
-        } 
-        if(puxar.aliases && Array.isArray(puxar.aliases))
-        puxar.aliases.forEach(x => client.aliases.set(x, puxar.name))
-    } 
+const comandos = fs.readdirSync(`./src/commands/${local}`).filter(arquivo => arquivo.endsWith('.js'))
+for(let file of comandos) {
+let puxar = require(`./src/commands/${local}/${file}`)
+if(puxar.name) {
+client.commands.set(puxar.name, puxar)
+}
+if(puxar.aliases && Array.isArray(puxar.aliases))
+puxar.aliases.forEach(x => client.aliases.set(x, puxar.name))
+} 
 });
 
 // event
