ee13fb38cc93aafb92129a2b6a8909cbd685fc96
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-05-25 12:09:36 -0300

    update CommandMeme.js

--- src/commands/commands/CommandMeme.js
+++ src/commands/commands/CommandMeme.js
@@ -1,1 +1,26 @@
-.
+//Importing the packages used in this command!
+const Discord = require("discord.js");
+const got = require('got');
+
+//Import module accepted by hadler!
+module.exports = {
+  name: "meme",
+  aliases: ["meme"],
+	run: async(message, args) => {
+  
+  //what makes meme command work has bug ;-;
+    const meme = new Discord.MessageEmbed()
+	  got('https://www.reddit.com/r/MemesBR/random/.json').then(response => {
+	 	const [list] = JSON.parse(response.body);
+		const [post] = list.data.children;
+		const permalink = post.data.permalink;
+		const memeUrl = `https://reddit.com${permalink}`;
+		const memeImage = post.data.url;
+  	meme.setColor('RANDOM')
+    meme.setImage(memeImage)
+		message.reply({ embeds: [meme] })
+    }).catch(console.error);
+    
+	}
+	//end of code? I think so!
+};
