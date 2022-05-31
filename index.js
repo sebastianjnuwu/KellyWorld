b924da8630a5944734da93cfc647d874d9e7dc4b
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-05-31 17:09:18 -0300

    update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const options = {
 const date = new Intl.DateTimeFormat([], options);
 
 //Useful information
-client.on("ready", () => {
+client.once("ready", () => {
  console.log(colors.cyan("[Info]")+` ${client.user.tag} foi iniciada em ${client.guilds.cache.size} sevidores!`);
  console.log(colors.cyan("[Info]")+` tendo acesso a ${client.channels.cache.size} canais!`);
  console.log(colors.cyan("[Info]")+` contendo ${client.users.cache.size} usuarios!`);
