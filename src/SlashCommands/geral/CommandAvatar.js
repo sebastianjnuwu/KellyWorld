228480b6387b7f67de12df6361d906c23fd1690f
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-06-01 17:18:17 -0300

    update CommandAvatar.js

--- src/SlashCommands/geral/CommandAvatar.js
+++ src/SlashCommands/geral/CommandAvatar.js
@@ -1,1 +1,31 @@
+//importing the packages that will be used!
+const Discord = require("discord.js");
 
+//Slash Commands export module
+module.exports = {
+    name: 'avatar',
+    description: "📷 show your avatar or that of your friends!",
+    options: [{
+        name:'usuario',
+        type: 'USER',
+        description: 'Seleciona um usuário',
+        required: false,
+    }],
+    run: async (client, interaction, options) => {
+       
+//we define the user!
+const user = interaction.options.geUser('usuario')) || interaction.member.user
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(`🖼️ Avatar de ${user.username}`)
+            .setColor('BLUE')
+            .setImage(user.displayAvatarURL({
+                dynamic: true,
+                size: 1024
+            }))
+        
+        await interaction.followUp({
+            embeds: [embed]
+        }); 
+    } //will be the end of the command? I think so!
+}
