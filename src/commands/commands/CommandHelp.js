f72ad68247755706b46e6ca083ac64861b5f8b78
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-06-01 13:19:51 -0300

    update CommandHelp.js

--- src/commands/commands/CommandHelp.js
+++ src/commands/commands/CommandHelp.js
@@ -12,7 +12,7 @@ module.exports = {
        .setColor("RANDOM")
        .setTitle('🔍 commandos!')
 	     .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
-       .setDescription('"Esta menina tem dois lados um é o sol o outro é a lua. Com o sol ela expressa felicidade e alegria. Com a lua ele expressa seu medo e tristeza."\n\nUma garota de 17 anos que busca a ajudar o próximo e a humanidade, com a sua disciplina ela ajudar manter seu seu servidor mais organizado e seguro para que os membros tenha um ambiente agradável!\n\n<:K_env:938833579981566043> Veja meus comandos clicando [aqui](https://github.com/sebastianjnuwu/KellyWorld#-commands)\n\n<:K_morri:927681449929359410> Encontrou algum bug? click [aqui](https://github.com/sebastianjnuwu/KellyWorld/issues)\n\n<:K_Aceito:943604636873523200>  Me ajude a ficar online! click [aqui](https://ko-fi.com/sebastianjnuwu)')
+       
        
       message.reply({ embeds: [help] });
   }
