5f941e79183031344444d4a122c555515a40a15f
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-06-01 13:55:35 -0300

    update CommandHelp.js

--- src/commands/commands/CommandHelp.js
+++ src/commands/commands/CommandHelp.js
@@ -15,8 +15,8 @@ module.exports = {
        .setDescription(`Ola ${mensage.author} meu nome é **KellyWorld**, mais você pode também me chamar de _"Kelly"_, eu sou um ser que sonha com a paz, Às vezes ela demora pra acontecer, às vezes não acontece. Mas você tem que acordar.`)
        .addFields(
 		{ name: '<:K_:947545349151653898> Quer ver algumas das coisas que eu fasso?', value: '"Com grandes poderes vêm grandes responsabilidades_"' },
-		{ name: 'Veja alguns dos meus comandos clicando [aqui]() são muitos não consigo lembrar de todos.', value: '\u200B' },
-		{ name: '<:K_env:938833579981566043> encontrou algum bug? Avise-me [aqui]()', value: '', inline: true },
+		{ name: 'Veja alguns dos meus comandos clicando [aqui](https://github.com/sebastianjnuwu/KellyWorld#-commands) são muitos não consigo lembrar de todos.', value: '\u200B' },
+		{ name: '<:K_env:938833579981566043> encontrou algum bug? Avise-me [aqui](https://github.com/sebastianjnuwu/KellyWorld/issues)', value: '', inline: true },
 	)
        
       message.reply({ embeds: [help] });
