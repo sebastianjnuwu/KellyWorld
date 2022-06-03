cdc2b8745ec4ab3c6140f693ac9313f3081d7e6c
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-06-03 11:36:20 -0300

    update index.js

--- index.js
+++ index.js
@@ -80,22 +80,6 @@ client.on("messageCreate", async (message) => {
   command.run(client, message, args)
 });
 
-/*
-//Message when the boy is mentioned he responds! 
-client.on("messageCreate", (message) => {
-  if (message.author.bot) return;
-  if (message.channel.type == "") return
-  if (message.content == `<@${client.user.id}>` || message.content == `<@!${client.user.id}>`) {
-  const mgs = [ 
-  `<:K_env:938833579981566043>`,
-  `<:K_zan:924366252024164363>`,
-  `<:K_rei:939165290846093392>`
-  ]; 
-  let mes = () => mgs[~~(Math.random() * mgs.length)];
-  message.reply(`${mes()}`);
-	}
-}); */
-
 //Ping system together with uptimerobot
 app.use((req, res, next) => {
 console.log(colors.yellow("[Info]")+` Ping recebido as ${date.format(new Date())}`);
