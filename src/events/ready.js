d37695004fd546065609f5c556b26ab787ac7fc4
SEBASTIAN JN ฅ^•ﻌ•^ฅ <101356556+sebastianjnuwu@users.noreply.github.com>
2022-06-03 14:57:27 +0000

    Up

---
+++ src/events/ready.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+const client = require('./../../index.js');
+
+//Useful information
+client.once("ready", () => {
+ console.log(colors.cyan("[Info]")+` ${client.user.tag} foi iniciada em ${client.guilds.cache.size} sevidores!`);
+ console.log(colors.cyan("[Info]")+` tendo acesso a ${client.channels.cache.size} canais!`);
+ console.log(colors.cyan("[Info]")+` contendo ${client.users.cache.size} usuarios!`);
+});
