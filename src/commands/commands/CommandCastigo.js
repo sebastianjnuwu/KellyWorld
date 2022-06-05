6905dc3f3b95e1185368325830d99fc1e10ea8f8
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-06-05 20:41:24 -0300

    update CommandCastigo.js

--- src/commands/commands/CommandCastigo.js
+++ src/commands/commands/CommandCastigo.js
@@ -9,58 +9,62 @@ module.exports = {
   async run(client, message, args) {
 
 //Time to delete messages
-const deletarMsgComTempo = (msg, segundos = 10) => setTimeout(() => msg.delete().catch(() => {}), segundos * 10);
-        
+const deletarMsgComTempo = (msg, segundos = 10) =>
+
+        setTimeout(() => msg.delete().catch(() => {}), segundos * 1000);
+
+
     if (!message.member.permissions.has('MODERATE_MEMBERS'))
-        return message.reply('Você não tem permissão para usar esse comando')
+        return message.channel
+            .send('Você não tem permissão para usar esse comando!')
             .then(deletarMsgComTempo);
 
     if (!message.guild.me.permissions.has('MODERATE_MEMBERS'))
-        return message.reply('Eu não tenho a permissão necessária para isso')
+        return message.channel
+            .send('Eu não tenho a permissão necessária para isso!')
             .then(deletarMsgComTempo);
 
-    const modoUso = 'K.castigo @user 2h `Flood`';
+    const modoUso = 'K.castigo `@user` `motivo`';
 
     /** @type {GuildMember} */
-
     const membro = message.mentions.members.first() ||
         message.guild.members.cache.get(args[0]) ||
         await message.guild.members.fetch(args[0]).catch(() => {});
 
- if(membro.id === message.author.id) return message.reply(`${message.author} **Você não pode banir a si mesmo.**`).then(deletarMsgComTempo);
-
-    if (!membro) return message.reply(modoUso).then(deletarMsgComTempo);
+    if (!membro) return message.channel.send(modoUso).then(deletarMsgComTempo);
 
     if (args.length < 2)
-        return message.reply(modoUso).then(deletarMsgComTempo);
-        
+        return message.channel.send(modoUso).then(deletarMsgComTempo);
+
     const tempo = ms(args[1]);
 
-    if (!tempo) return message.reply(modoUso).then(deletarMsgComTempo);
+    if (!tempo) return message.channel.send(modoUso).then(deletarMsgComTempo);
 
     const motivo = args.slice(2) || 'Sem motivo';
 
     const botNaoConseguePunir = message.guild.me.roles.highest
         .comparePositionTo(membro.roles.highest) < 0;
-        
+
     if (botNaoConseguePunir)
-        return message.reply(`O cargo de ${membro.displayName} é maior do que o meu`)
+        return message.channel
+            .send(`O cargo de ${membro.displayName} é maior do que o meu!`)
             .then(deletarMsgComTempo);
 
     if (membro.isCommunicationDisabled())
-        return message.reply('Esse user já está em timeout').then(deletarMsgComTempo);
+        return message.reply('Esse user já está em timeout!');
 
     try {
-
     /** @type { GuildMember } */
-
-   const membroEmTimeout = await membro.timeout(tempo, motivo);
-   message.reply(`${membroEmTimeout} está em timeout até <t:${~~( membroEmTimeout.communicationDisabledUntilTimestamp / 1000)}>`).then(deletarMsgComTempo);
+        const membroEmTimeout = await membro.timeout(tempo, motivo);
+        message.reply(
+            `${membroEmTimeout} está em timeout até <t:${~~(
+                membroEmTimeout.communicationDisabledUntilTimestamp / 1000
+            )}>`
+        );
     }
     catch (error) {
-        message.reply('Erro ao aplicar punição').then(deletarMsgComTempo);
+        message.reply('Erro ao aplicar punição!');
         console.log(error);
-
     }
   }
   //end of code I think so! ugh
