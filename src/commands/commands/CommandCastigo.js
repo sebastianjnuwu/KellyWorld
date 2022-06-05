69d34b23d14a03ff3b9415f438e1d753dd976a32
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-06-05 20:35:29 -0300

    update CommandCastigo.js

--- src/commands/commands/CommandCastigo.js
+++ src/commands/commands/CommandCastigo.js
@@ -9,7 +9,7 @@ module.exports = {
   async run(client, message, args) {
 
 //Time to delete messages
-const deletarMsgComTempo = (msg, segundos = 10) => setTimeout(() => msg.delete().catch(() => {}), segundos * 1000);
+const deletarMsgComTempo = (msg, segundos = 10) => setTimeout(() => msg.delete().catch(() => {}), segundos * 10);
         
     if (!message.member.permissions.has('MODERATE_MEMBERS'))
         return message.reply('Você não tem permissão para usar esse comando')
