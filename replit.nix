79e2be66a22305c1cfebdde6b20117d38289ff21
SEBASTIAN JN ฅ^•ﻌ•^ฅ <101356556+sebastianjnuwu@users.noreply.github.com>
2022-06-06 09:29:13 +0000

    `up`

--- replit.nix
+++ replit.nix
@@ -1,8 +1,8 @@
 { pkgs }: {
 	deps = [
 		pkgs.nodejs-16_x
-        pkgs.nodePackages.typescript-language-server
-        pkgs.nodePackages.yarn
-        pkgs.replitPackages.jest
+    pkgs.nodePackages.typescript-language-server
+    pkgs.nodePackages.yarn
+    pkgs.replitPackages.jest
 	];
 }
