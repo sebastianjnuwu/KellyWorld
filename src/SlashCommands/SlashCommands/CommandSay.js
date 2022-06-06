c13fc00cce045fff33029d2d3b5f666c0f9f8497
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-06-06 15:06:00 -0300

    update CommandSay.js

--- src/SlashCommands/SlashCommands/CommandSay.js
+++ src/SlashCommands/SlashCommands/CommandSay.js
@@ -26,7 +26,7 @@ module.exports = {
         let canal = interaction.options.getChannel('canal') || interaction.channel;
       try {
 
-if(!interaction.member.permissions.has("MANAGE_MESSAGES")) return interaction.reply({ content: "Você não tem permissão para usar este comando!", ephemeral: true})
+if(!interaction.member.permissions.has("MANAGE_MESSAGES")) return interaction.reply({ content: "<:K_negado:943604703378415688> | Você não tem permissão para usar este comando!", ephemeral: true})
   
 
         interaction.reply({ content: `Anúncio enviado com sucesso no canal: <#${canal.id}>`, ephemeral: true })
@@ -35,10 +35,10 @@ if(!interaction.member.permissions.has("MANAGE_MESSAGES")) return interaction.re
 
   } catch (err) {
 
-        let dev_don = new Discord.MessageEmbed()
-        .setDescription(`:noo: | ${interaction.user} Opss... algo de errado não está certo. User \`/reportbug\` para reportar! `)
+        let Erro = new Discord.MessageEmbed()
+        .setDescription(`<:K_negado:943604703378415688> | ${interaction.user} Opss... algo de errado não está certo.`)
         .setColor("#36393e")
-        interaction.reply({embeds: [dev_don]});
+        interaction.reply({embeds: [Erro]});
         
     }
  }
