509a2e79fc1cfb5ebdd16113df96e5718825951a
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-06-06 10:27:12 -0300

    update CommandSay.js

--- src/SlashCommands/SlashCommands/CommandSay.js
+++ src/SlashCommands/SlashCommands/CommandSay.js
@@ -1,6 +1,7 @@
 //importing the packages that will be used!
 const Discord = require("discord.js");
 
+//import module
 module.exports =  {
   name: "say", 
   description: "🗣️ talk in a chat using the bot", 
@@ -13,12 +14,16 @@ module.exports =  {
     }],
   run: async (client, args, options, interaction) => {
   
+  //permission the bot needs to use the command
   if (!message.guild.me.permissions.has('ADMINISTRATOR'))
        return interaction.reply({ content: "<:K_negado:943604703378415688> Eu preciso da permissão de `ADMINISTRADOR` para executar este comando!", ephemeral: true });
   
+  //the necessary permission that the member must have
   if (!interaction.member.permissions.has("ADMINISTRATOR")) 
       return interaction.reply({ content: "<:K_negado:943604703378415688> você não tem a permissão necessaria para usar este comando, você precisa ter a permissão de `ADMINISTRADOR`", ephemeral: true });
   
+  //we define the message that will be quoted
+  let mensagem = interaction.options.getString("mensagem");
   
   }
 }
