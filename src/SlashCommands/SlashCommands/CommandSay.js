7dab1e22d876b7fd913e1b7e795f98b3b377183d
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-06-06 17:07:47 -0300

    update CommandSay.js

--- src/SlashCommands/SlashCommands/CommandSay.js
+++ src/SlashCommands/SlashCommands/CommandSay.js
@@ -1,6 +1,8 @@
+//importing used packages
 const { MessageEmbed, version, CommandInteraction, Client } = require("discord.js");
 const Discord = require("discord.js")
 
+//import module supported by hadler
 module.exports = {
     name: 'say',
     description:  '🗣️ message using Kelly in a channel',
@@ -18,22 +20,25 @@ module.exports = {
             description: 'coloque a mensagem a ser enviada!',
             type: 'STRING',
             required: true,
-        }
-    ],
+       }],
     run: async (client, interaction, args) => {
-
-  let description = interaction.options.getString("mensagem");
-  
+   
+  //we set the message and channel variable
+  let mensagem  = interaction.options.getString("mensagem");
   let canal = interaction.options.getChannel('canal') || interaction.channel;
   
   try {
 
-if(!interaction.member.permissions.has("MANAGE_MESSAGES")) return interaction.reply({ content: "<:K_negado:943604703378415688> | Você não tem permissão para usar este comando!", ephemeral: true})
+//first If saying that the user does not have permission
+if(!interaction.member.permissions.has("MANAGE_MESSAGES")) return interaction.reply({ content: "<:K_negado:943604703378415688> | Você não tem permissão para usar este comando!", ephemeral: true});
 
+ //saying it sent the message to x channel 
   interaction.reply({ content: `${interaction.user} Anúncio enviado com sucesso no canal: <#${canal.id}>`, ephemeral: true })
-        
+    
+    //sending the message to the requested channel!
     canal.send({content: `${mensagem}`})
 
+  //if there is any mistake
   } catch (err) {
 
    let Erro = new Discord.MessageEmbed()
@@ -42,5 +47,5 @@ if(!interaction.member.permissions.has("MANAGE_MESSAGES")) return interaction.re
         interaction.reply({embeds: [Erro]});
         
     }
- }
+ } //finally finished the code lol it took a little work
 } 
