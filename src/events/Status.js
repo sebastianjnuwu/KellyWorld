0bb1d9bc321bec481292e4fbec48dff5b5a8c12f
SEBASTIAN JN ฅ^•ﻌ•^ฅ <101356556+sebastianjnuwu@users.noreply.github.com>
2022-06-07 15:31:19 +0000

    Up

---
+++ src/events/Status.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+const client = require('./../../index.js');
+
+//Activity status of our bot
+client.on("ready", () => {
+ let activities = ["Minecraft em Kettra World 🌟"];
+ let i = 0;
+ setInterval(() => 
+ client.user.setActivity(`${activities[i++ % activities.length]}`, { type: "PLAYING", url: "https://www.twitch.tv/sebastianjnuwu" }), 8000); 
+  client.user.setStatus("dnd");
+});
