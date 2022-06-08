c17653aadd6afd2cbf07d9bb5b90b3f348f49a4d
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-06-08 11:40:47 -0300

    update CommandClear.js

--- src/SlashCommands/SlashCommands/CommandClear.js
+++ src/SlashCommands/SlashCommands/CommandClear.js
@@ -16,11 +16,11 @@ module.exports = {
     
     let delamount = interaction.options.getNumber('quantidade');
 
-    if (!interaction.member.permissions.has("MANAGE_MESSAGES")) return interaction.reply({ content: `:K_negado: | Você não possui permissão para utilizar este comando.`, ephemeral: true });
+    if (!interaction.member.permissions.has("MANAGE_MESSAGES")) return interaction.reply({ content: `<:K_negado:943604703378415688> | Você não possui permissão para utilizar este comando.`, ephemeral: true });
     
-    if (delamount <= 0) return interaction.reply({ content: `Voce pode apagar apenas de 1 a 99 mensagens`, ephemeral: true });
+    if (delamount <= 0) return interaction.reply({ content: `<:K_negado:943604703378415688> | Voce pode apagar apenas de 1 a 99 mensagens`, ephemeral: true });
 
-    if (parseInt(delamount) > 99) return interaction.reply({ content: `Voce pode apagar apenas de 1 a 99 mensagens`, ephemeral: true });
+    if (parseInt(delamount) > 99) return interaction.reply({ content: `<:K_negado:943604703378415688> | Voce pode apagar apenas de 1 a 99 mensagens`, ephemeral: true });
 
     await interaction.channel.bulkDelete(parseInt(delamount), true);
     interaction.reply({ content: `o chat teve ${delamount} apagadas!`, ephemeral: true });
