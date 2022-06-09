830729fcb60da025d8fac70db01de4cba980ea23
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-06-09 14:59:41 -0300

    update T.js

--- src/SlashCommands/SlashCommands/T.js
+++ src/SlashCommands/SlashCommands/T.js
@@ -33,25 +33,24 @@ module.exports = {
    
   let motivo = interaction.options.getString("motivo") || `Você não inseriu um motivo.`;
  
- if (!interaction.guild.me.permissions.has("MODERATE_MEMBERS")) return interaction.reply({ content: "<:K_negado:943604703378415688> | eu não tenho a permissão de `ADMINISTRADOR`.....", ephemeral: true });
+ if(!interaction.guild.me.permissions.has("MODERATE_MEMBERS")) return interaction.reply({ content: "<:K_negado:943604703378415688> | eu não tenho a permissão de `ADMINISTRADOR`.....", ephemeral: true });
  
- if (!interaction.member.permissions.has("MODERATE_MEMBERS")) return          interaction.reply({ content: "<:K_negado:943604703378415688> | Você não possui permissão para utilizar este comando, permissão necessária `MODERATE_MEMBERS`...", ephemeral: true });
+ if(!interaction.member.permissions.has("MODERATE_MEMBERS")) return          interaction.reply({ content: "<:K_negado:943604703378415688> | Você não possui permissão para utilizar este comando, permissão necessária `MODERATE_MEMBERS`...", ephemeral: true });
  
  if(t <= 0) return interaction.reply({ content: `<:K_negado:943604703378415688> | Não existe minutos menores que 0!`, ephemeral: true });
 
-  let tempo = ms(`${t}m`);
+ let tempo = ms(`${t}m`);
    
-   if(user.id === client.user.id) return interaction.reply({ content: "Por que você quer me silenciar?", ephemeral: true });
+ if(user.id === client.user.id) return interaction.reply({ content: "Por que?", ephemeral: true });
     
-   if(user.id === interaction.user.id) return interaction.reply({ content: "Você não pode se silenciar!", ephemeral: true });
+ if(user.id === interaction.user.id) return interaction.reply({ content: "Você não pode se castigar!", ephemeral: true });
     
-   if(user.roles.highest.position > interaction.guild.me.roles.highest.position ) return interaction.reply({ content: "<:K_negado:943604703378415688> | o cargo do usuario é maior que o meu!", ephemeral: true });
+ if(user.roles.highest.position > interaction.guild.me.roles.highest.position ) return interaction.reply({ content: "<:K_negado:943604703378415688> | o cargo do usuario é maior que o meu!", ephemeral: true });
    
-   if(user.isCommunicationDisabled()) return interaction.reply({ content: "<:K_negado:943604703378415688> | este membro já estar ssilenciado!", ephemeral: true });
+   if(user.isCommunicationDisabled()) return interaction.reply({ content: "<:K_negado:943604703378415688> | este membro já estar silenciado!", ephemeral: true });
 
  user.timeout(tempo, motivo).catch(() => {});
 
- interaction.reply({ content: `${user} foi silenciado por ${tempo} minuto(s)!`});
-
+interaction.reply({ content: `${user} foi silenciado por ${tempo} minuto(s)!`, ephemeral: true });
     }
 };
