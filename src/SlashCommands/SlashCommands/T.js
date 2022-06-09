e54c089cfec4db0441970dc2b94bf487dafb889b
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-06-09 14:54:24 -0300

    update T.js

--- src/SlashCommands/SlashCommands/T.js
+++ src/SlashCommands/SlashCommands/T.js
@@ -5,43 +5,42 @@ const ms = require('ms');
 //command import using with hadler
 module.exports = {
     name: 'castigo',
-    description: '⛓️ quer silenciar alguém?',
+    description: '⛓️ quer castigar alguém?',
     type: 'CHAT_INPUT',
     options: [{
         name: 'usuario',
         type: 'USER',
-        description: 'Mencione um usuário para ser ssilenciado!',
+        description: 'Mencione um usuário para ser castigado!',
         required: true,
     },
     {
         name: 'tempo',
-        description: 'quantidade de tempo em minutos',
+        description: 'tempo em minutos para o usuário sair do castigo!',
         type: 'NUMBER',
         required: true,
     },
     {
         name: "motivo",
         type: 'STRING',
-        description: "Seleciona o motivodo banimento desse usuario.",
+        description: "Seleciona o motivo do castigo dedea usuário!.",
         required: false,
         }],
-    run: async (client, interaction, options) => {
+  run: async (client, interaction, options) => {
 
+  let user = interaction.options.getMember('usuario');
    
-   let user = interaction.options.getMember('usuario');
+  let t = interaction.options.getNumber('tempo');
    
-   let t = interaction.options.getNumber('tempo');
-   
-   if (t <= 0) return interaction.reply({ content: `<:K_negado:943604703378415688> | Não existe minutos !enores que 0!`, ephemeral: true });
+  let motivo = interaction.options.getString("motivo") || `Você não inseriu um motivo.`;
+ 
+ if (!interaction.guild.me.permissions.has("MODERATE_MEMBERS")) return interaction.reply({ content: "<:K_negado:943604703378415688> | eu não tenho a permissão de `ADMINISTRADOR`.....", ephemeral: true });
+ 
+ if (!interaction.member.permissions.has("MODERATE_MEMBERS")) return          interaction.reply({ content: "<:K_negado:943604703378415688> | Você não possui permissão para utilizar este comando, permissão necessária `MODERATE_MEMBERS`...", ephemeral: true });
+ 
+ if(t <= 0) return interaction.reply({ content: `<:K_negado:943604703378415688> | Não existe minutos menores que 0!`, ephemeral: true });
 
-   let tempo = ms(`${t}m`);
+  let tempo = ms(`${t}m`);
    
-   let motivo = interaction.options.getString("motivo") || `Você não inseriu um motivo.`;
-   
-   if (!interaction.member.permissions.has("MODERATE_MEMBERS")) return          interaction.reply({ content: "<:K_negado:943604703378415688> | Você não possui permissão para utilizar este comando.", ephemeral: true });
-    
-   if (!interaction.guild.me.permissions.has("MODERATE_MEMBERS")) return interaction.reply({ content: "<:K_negado:943604703378415688> | eu não tenho a permissão `MODERATE_MEMBERS`, como você me convida para seu servidor e não me dar as permissões nenecessária 7-7", ephemeral: true });
-     
    if(user.id === client.user.id) return interaction.reply({ content: "Por que você quer me silenciar?", ephemeral: true });
     
    if(user.id === interaction.user.id) return interaction.reply({ content: "Você não pode se silenciar!", ephemeral: true });
