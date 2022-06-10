df9b1511d247093f63ebcf0b93541dc722703396
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-06-10 13:47:27 -0300

    update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,8 +1,12 @@
 { pkgs }: {
-	deps = [
-		pkgs.nodejs-16_x
-    pkgs.nodePackages.typescript-language-server
-    pkgs.nodePackages.yarn
-    pkgs.replitPackages.jest
-	];
-}
+    deps = [
+        pkgs.nodejs-16_x
+        pkgs.nodePackages.typescript-language-server
+        pkgs.nodePackages.yarn
+        pkgs.replitPackages.jest
+    pkgs.libuuid
+    ];
+  env = {
+LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid];
+};
+} 
