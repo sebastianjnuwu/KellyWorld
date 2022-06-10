cbf545273d3eb2403415ae7f2b10d11013710eb5
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-06-10 17:31:13 -0300

    update CommandClear.js

--- src/SlashCommands/SlashCommands/CommandClear.js
+++ src/SlashCommands/SlashCommands/CommandClear.js
@@ -1,7 +1,7 @@
-// listing the packages used!
+// importing the packages/libraries used this command!
 const Discord = require('discord.js')
 
-//módulo de importação de comandos em slash
+// slash command import module supported by hadler.
 module.exports = {
     name: 'clear',
     description: '🗑️ limpar as mensagens do canal?',
@@ -12,20 +12,28 @@ module.exports = {
         type: 'NUMBER',
         required: true,
     }],
-   run: async (client, interaction, options) => {
-    
-    let delamount = interaction.options.getNumber('quantidade');
+  run: async (client, interaction, options) => {
+  
+  //we set the variable of how many messages to delete!
+  let delamount = interaction.options.getNumber('quantidade');
 
-    if(!interaction.guild.me.permissions.has("ADMINISTRATOR")) return interaction.reply({ content: "<:K_negado:943604703378415688> | eu não tenho a permissão de `ADMINISTRADOR`.....", ephemeral: true });
-    
-    if (!interaction.member.permissions.has("MANAGE_MESSAGES")) return interaction.reply({ content: `<:K_negado:943604703378415688> | Você não possui permissão para utilizar este comando.`, ephemeral: true });
-    
-    if (delamount <= 0) return interaction.reply({ content: `<:K_negado:943604703378415688> | Voce pode apagar apenas de 1 a 99 mensagens`, ephemeral: true });
+  // we define an if that checks if the bot has the necessary permission to execute the command!
+  if(!interaction.guild.me.permissions.has("ADMINISTRATOR")) return interaction.reply({ content: "<:K_negado:943604703378415688> | eu não tenho a permissão de `ADMINISTRADOR`.....", ephemeral: true });
+  
+  // we define an if that checks if the user has the necessary permission to execute the command!
+  if(!interaction.member.permissions.has("MANAGE_MESSAGES")) return interaction.reply({ content: `<:K_negado:943604703378415688> | Você não possui permissão para utilizar este comando.`, ephemeral: true });
+  
+  // we define an if to slash the number 0 or less! 
+  if(delamount <= 0) return interaction.reply({ content: `<:K_negado:943604703378415688> | Voce pode apagar apenas de 1 a 99 mensagens`, ephemeral: true });
 
-    if (parseInt(delamount) > 99) return interaction.reply({ content: `<:K_negado:943604703378415688> | Voce pode apagar apenas de 1 a 99 mensagens`, ephemeral: true });
+ // we define an if if the messages to be deleted exceed 99.
+  if(parseInt(delamount) > 99) return interaction.reply({ content: `<:K_negado:943604703378415688> | Voce pode apagar apenas de 1 a 99 mensagens`, ephemeral: true });
+ 
+ // finally delete the channel messages according to the amount
+  await interaction.channel.bulkDelete(parseInt(delamount), true);
+  
+  // sends a message that only the user who executed the command can see!
+  interaction.reply({ content: `o chat teve ${delamount} mensagens apagadas!`, ephemeral: true });
 
-    await interaction.channel.bulkDelete(parseInt(delamount), true);
-    interaction.reply({ content: `o chat teve ${delamount} apagadas!`, ephemeral: true });
-
-   }
+  }
 };
