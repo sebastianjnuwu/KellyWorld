87e03bbc2d3fd76acdc23b70c393f4712a858b47
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-06-10 15:02:58 -0300

    update CommandBan.js

--- src/SlashCommands/SlashCommands/CommandBan.js
+++ src/SlashCommands/SlashCommands/CommandBan.js
@@ -24,9 +24,9 @@ module.exports = {
    
    let motivo = interaction.options.getString("motivo") || `Você não inseriu um motivo.`;
 
-if(!interaction.guild.me.permissions.has("ADMINISTRATOR")) return interaction.reply({ content: "<:K_negado:943604703378415688> | eu não tenho a permissão de `ADMINISTRADOR`.....", ephemeral: true });
+   if(!interaction.guild.me.permissions.has("ADMINISTRATOR")) return interaction.reply({ content: "<:K_negado:943604703378415688> | eu não tenho a permissão de `ADMINISTRADOR`.....", ephemeral: true });
 
-   if (!interaction.member.permissions.has("BAN_MEMBERS")) return          interaction.reply({ content: "<:K_negado:943604703378415688> | Você não possui permissão para utilizar este comando.", ephemeral: true });
+   if(!interaction.member.permissions.has("BAN_MEMBERS")) return          interaction.reply({ content: "<:K_negado:943604703378415688> | Você não possui permissão para utilizar este comando.", ephemeral: true });
      
    if(user.id === interaction.guild.ownerId) return interaction.reply({ content: "<:K_negado:943604703378415688> | eu não posso banir o dono(a) do servidor!", ephemeral: true });
 
