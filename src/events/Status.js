97c3745c3a75102268418721938422ea398e064c
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-06-15 12:03:45 -0300

    update Status.js

--- src/events/Status.js
+++ src/events/Status.js
@@ -3,7 +3,7 @@ const client = require('./../../index.js');
 
 //Activity status of our bot
 client.on("ready", () => {
- let activities = ["Minecraft em Kettra World 🌟"];
+ let activities = ["Minecraft em Kettra World 🌟 | Cluster UwU"];
  let i = 0;
  setInterval(() => 
  client.user.setActivity(`${activities[i++ % activities.length]}`, { type: "PLAYING", url: "https://www.twitch.tv/sebastianjnuwu" }), 8000); 
