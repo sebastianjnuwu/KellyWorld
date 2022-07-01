56fafe0c901125c94cab87a421ba39b082fd2b85
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-07-01 11:39:08 -0200

    update Kelly.js

--- Kelly.js
+++ Kelly.js
@@ -2,14 +2,14 @@
 import { load } from 'js-yaml';
 import colors from 'colors';
 import { readFileSync } from 'node:fs';
-import { AutoPoster } from 'topgg-autoposter';
+//import { AutoPoster } from 'topgg-autoposter';
 import KellyWorld from './src/Client.js';
 //global.config = load(readFileSync('./config.yml', 'utf8'));
 
 const client = new KellyWorld();
 client.start();
 // AutoPoster(global.config.connections.topgg, client);
-AutoPoster(process.env.topgg, client);
+//AutoPoster(process.env.topgg, client);
 
 const trainerror = (error) => {
   if (error.toString().includes('Missing Permissions') || error.toString().includes('Missing acess')) return;
