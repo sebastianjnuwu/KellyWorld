92d2f60c1568cb174fd7dfbdfcf829b7981e2bfe
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-07-02 13:50:03 -0200

    adding bot status and activity

--- src/Events/Client/Ready.js
+++ src/Events/Client/Ready.js
@@ -4,12 +4,12 @@ export default {
   name: 'ready',
   async exec(client,message) {
   
-  // console message when bot is powered on!
+  // console message when bot is powered on...
  console.log(colors.brightGreen("[Info]")+` ${client.user.tag} was started in ${client.guilds.cache.size} servers!`);
  console.log(colors.brightGreen("[Info]")+` having access to ${client.channels.cache.size} channels!`);
  console.log(colors.brightGreen("[Info]")+` containing ${client.users.cache.size} users!`);
  
- // bot activity status
+ // bot activity status...
 let list = [`KettraWorld - New Prefix: Kelly`, `KettraWorld - Jogando Pokémon`,`KettraWorld - R.I.P`,`KettraWorld - ouvindo música`];
 let status = list[Math.floor(Math.random() * list.length)];
 setInterval(() => client.user.setActivity(`${status}`, { type: 1 }), 3 * 30000);
