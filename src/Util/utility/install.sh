23166e4ffcec26a1bb477997ac15f7a451c5a10c
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-07-06 07:40:25 -0300

    update install.sh

--- src/Util/utility/install.sh
+++ src/Util/utility/install.sh
@@ -1,2 +1,2 @@
-sudo apt update -y && sudo apt upgrade -y && clear
-echo "updating packages!"
+echo "this script was made to run in termux."
+pkg update -y && pkg upgrade -y && pkg install nodejs -y && pkg install yarn -y && clear && echo "nodejs successfully downloaded together with yarn!" 
