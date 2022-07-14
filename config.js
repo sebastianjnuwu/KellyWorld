492520b1233943fe78d9b219568684364333e212
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-07-14 14:49:23 -0300

    `7-7`

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export default {
     token: process.env.token
   },
   owners: {
-    user: ['932678185970192404','591437825790967836','463384487569522689','953757224528928768']
+    user: ['932678185970192404','591437825790967836','463384487569522689']
   },
   connections: {
     mongodb: process.env.mongodb 
