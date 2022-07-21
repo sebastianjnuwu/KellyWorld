3c3c1ec18116231666d5ee448461c257927b8e28
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-07-21 18:07:02 -0300

    update messageUpdate.js

--- src/Events/Client/messageUpdate.js
+++ src/Events/Client/messageUpdate.js
@@ -1,10 +1,8 @@
 export default {
   name: 'messageUpdate',
   async exec(client, oldMessage, message) {
-  
-  if(!message.guild || !message.author ||message.author?.bot) return;
+    if(!message.guild || !message.author ||message.author?.bot) return;
 
-  if (message?.content.trim() !== oldMessage?.content.trim()) client.emit('messageCreate', message);
-  
+    if (message?.content.trim() !== oldMessage?.content.trim()) client.emit('messageCreate', message);
   }
 };
