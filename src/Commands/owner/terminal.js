580414e5aefdcc6ac1c6808b5055cda2f5743abc
sebastianjnuwu <sebastianjnuwu@gmail.com>
2022-07-23 19:32:34 -0300

    feat: refactor(everyone): 'fixing common mistakes'

--- src/Commands/owner/terminal.js
+++ src/Commands/owner/terminal.js
@@ -1,5 +1,5 @@
 import { exec } from 'child_process';
-
+// eslint-disable-next-line no-control-regex
 const ANSI_REGEX = /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g;
 export default {
   name: 'shell',
