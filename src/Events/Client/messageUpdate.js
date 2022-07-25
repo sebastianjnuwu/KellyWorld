5f4fe42a74ce81fdb8c820a3b87178797d4406a8
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-07-25 20:54:56 -0300

    back: Going back to how it was before.

--- src/Events/Client/messageUpdate.js
+++ src/Events/Client/messageUpdate.js
@@ -1,8 +1,10 @@
+
 export default {
   name: 'messageUpdate',
   async exec(client, oldMessage, message) {
-    if(!message.guild || !message.author ||message.author?.bot) return;
+  
+  if (!message.guild || !message.author || message.author?.bot) return;
 
-    if (message?.content.trim() !== oldMessage?.content.trim()) client.emit('messageCreate', message);
+  if (message.content?.trim() !== oldMessage.content?.trim()) client.emit('messageCreate', message);
   }
 };
