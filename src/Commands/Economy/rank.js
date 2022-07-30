b1082cfa4337d55be3c0110e83f3117ed1ec18fc
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-07-30 19:07:42 -0300

    adding: 5 places in the leaderboard from now on!

--- src/Commands/Economy/rank.js
+++ src/Commands/Economy/rank.js
@@ -8,7 +8,7 @@ export default {
    
     db.sort((a,b) => (b.economy.kerein) - (a.economy.kerein));
    
-    db = db.slice(0,3);
+    db = db.slice(0,5);
    
     message.reply(`<:K_coin:939160141675262022> **Os Kelionarios do sub mundo de kettra**\n> ${db.map((player, i) => `#${i+1} | 👥 _${client.users.cache.get(player.id).username}_ - **${abreviar(player.economy.kerein)} KR**`).join('\n> ')}`);
   }
