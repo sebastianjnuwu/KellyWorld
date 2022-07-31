4fa678fa4829e10ffb94f74c39454870b6b8f3c7
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-07-31 12:00:00 -0300

    leaving the game

--- src/Commands/Economy/bet.js
+++ src/Commands/Economy/bet.js
@@ -28,7 +28,7 @@ export default {
     let won = 2 * args[0];
     let bet = Math.floor(Math.random() * 10);
 
-    if(bet > 8) {
+    if(bet > 6) {
       await client.db.user.updateOne({  _id: player.id },
         { $set: { 'economy.kerein': db.economy.kerein + won }});
       return message.reply(`${t('commands:Bet.won',{ ganhou: String(won)})}`);
