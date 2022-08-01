f152035915c920b60a03e4a3f1b61e2877f80ba0
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-08-01 13:22:49 -0300

    feat: tidying up and leaving some comments!

--- src/Events/Client/messageCreate.js
+++ src/Events/Client/messageCreate.js
@@ -1,61 +1,78 @@
+// importing the necessary packages.
 import i18next from 'i18next';
 
+// exporting the event 
 export default {
   name: 'messageCreate',
   async exec(client, message) {
   
-    if (message.author.bot || !message.guild) return;
-    
-    let prefix;
+  // bot's don't use commands or run nana dm do bot!
+  if (message.author.bot || !message.guild) return;
   
-    let USER = await client.db.user.findOne({
+  // define the prefix?
+  let prefix;
+  
+  // looking up user information in the database.
+  let USER = await client.db.user.findOne({
       _id: message.author.id,
-    });
+  });
     
-    let GUILD = await client.db.guild.findOne({
+  // fetching information from that server in the database.
+  let GUILD = await client.db.guild.findOne({
       _id: message.guild.id,
-    });
-
-    if (!GUILD) GUILD = await client.db.guild.create({
+  });
+  
+  // in case the server is not in the database now be!
+  if (!GUILD) GUILD = await client.db.guild.create({
       _id: message.guild.id,
-    });
-
-    if (message.content.toLowerCase().startsWith('kelly')) {
+  });
+  
+  // we take the prefix like "Kelly" or "&" which is the default!
+  if (message.content.toLowerCase().startsWith('kelly')) {
       prefix = 'kelly';
-    } else {
+  } else {
       prefix = GUILD.prefix;
-    }
+  }
   
-    if (!message.content.toLowerCase().startsWith(prefix)) return;
+  // taking the prefix in tiny
+  if (!message.content.toLowerCase().startsWith(prefix)) return;
   
-    if (!USER) USER = await client.db.user.create({ _id: message.author.id });
+  // if the user does not have it in the database now it will be!
+  if (!USER) USER = await client.db.user.create({ _id: message.author.id });
   
-    const [cmd, ...args] = message.content.slice(prefix.length).trim().split(/ +/g);
+  // taking the prefix along with the word in front!
+  const [cmd, ...args] = message.content.slice(prefix.length).trim().split(/ +/g);
   
-    let t = GUILD.lang || 1;
+  // variable "t" is for language, the default language is Portuguese!
+  let t = GUILD.lang || 1;
  
-    switch(t) {
-    case 1:
-      t = i18next.getFixedT('pt-BR');
-      break;
-    case 2:
-      t = i18next.getFixedT('en-US');
-      break;
-    }
-
-    if (cmd.length === 0) return;
-
-    const command = client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd));
-
-    if (!command) return;
+ // varieval "t" has a value like this matching the following
+  switch(t) {
+  case 1:
+  t = i18next.getFixedT('pt-BR');
+    break;
+  case 2:
+  t = i18next.getFixedT('en-US');
+    break;
+  }
   
-    if(command.ownerOnly && !client.owners.some(id => id === message.author.id)) return;
-   
-   if(command.eco && !client.channel.some(id => id === message.channel.id)) return message.reply(`Ops, Felizmente so posso executar comandos no chat <#894002116446806066>`);
-   
-    const d = msg => setTimeout(() => { msg.delete().catch(() => {}); message.delete().catch(() => {});}, 10000);
-    
-    await command.exec({ client, message, args, d, t });
+  // if you just put the prefix, don't return anything!
+  if (cmd.length === 0) return;
+  
+  // looking for his command and alias!
+  const command = client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd));
+
+  // if the command does not exist, it does not return anything!
+  if (!command) return;
+  
+  // to drill commands for common users (only important people can execute)
+  if (command.ownerOnly && !client.owners.some(id => id === message.author.id)) return;
+  
+  // d varieval to delete message in certain time
+  const d = msg => setTimeout(() => { msg.delete().catch(() => {}); message.delete().catch(() => {});}, 10000);
+  
+  // executing command and the accompanying variables!
+  await command.exec({ client, message, args, d, t });
  
   }
 };
