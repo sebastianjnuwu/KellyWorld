a61270fb349cb87628064d5a6b10dae61d50c488
SEBASTIAN JN <sebastianjnuwu@gmail.com>
2022-09-23 15:40:13 -0300

    update antiinvite.js

--- src/Commands/Moderation/antiinvite.js
+++ src/Commands/Moderation/antiinvite.js
@@ -9,7 +9,7 @@ export default {
         
     if (!message.member.permissions.has('ManageGuild') && !client.owners.some(id => id === message.author.id)) return message.reply(`${t('commands:UserPermissions.ManageGuild',{ user: String(message.author)})}`).then(d);
   
-    if (!args[0] || !['off','on','desativar', 'disable', 'ativar', 'enable'].some((x) => x == args[0].toLowerCase())) return message.reply(`${t('commands:antiinvite.Message')}`).then(d);
+    if (!args[0] || !['off','on','desativar', 'disable', 'ativar', 'enable'].some((x) => x == args[0].toLowerCase())) return message.reply(`${t('TEXT:antiinvite.information')}`).then(d);
     
     if (['on','ativar','enable'].some((x) => x == args[0].toLowerCase())) {
       await client.db.guild.findOneAndUpdate({ _id: message.guild.id },{ $set: { 'antiinvite': true } });
