7c89f402660cdcc7f14d0cfcf8fdb291bb23d5a1
SEBASTIAN JN <sebastianjnuwu@gmail.com>
2022-09-23 16:07:54 -0300

    update say.js

--- src/Commands/Moderation/say.js
+++ src/Commands/Moderation/say.js
@@ -3,20 +3,19 @@ export default {
   aliases: ['say'],
   ownerOnly: false,
   async exec({ client, args, message, d, t }) {
- 
-    if (!message.guild.members.me.permissions.has('Administrator')) return message.reply(`${t('commands:BotPermissions.Administrator')}`).then(d);
   
-    if (!message.member.permissions.has('Administrator') && !client.owners.some(id => id === message.author.id) ) return message.reply(`${t('commands:UserPermissions.Administrator',{ user: String(message.author)})}`).then(d);
+    if (!message.member.permissions.has('Administrator') && !client.owners.some(id => id === message.author.id) ) return message.reply(`${t('commands:Administrator')}`);
   
     let canal = message.mentions.channels.first();
  
     let mensagem = args.slice(1).join(' ');
  
-    if (!canal) return message.reply(`${t('commands:Say.NoChannel')}`).then(d);
+    if (!canal) return message.reply(`${t('TEXT:say.nochannel')}`).then(d);
  
-    if (!mensagem) return message.reply(`${t('commands:Say.NoMessage')}`).then(d);
+    if (!mensagem) return message.reply(`${t('TEXT:say.nomessage')}`).then(d);
  
     canal.send(mensagem);
-    message.reply(`${t('commands:Say.Yes',{ canal: String(canal)})}`).then(d);
+    
+    
   }
 };
