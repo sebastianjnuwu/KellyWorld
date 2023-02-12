68c988de166ebafa103384675c3dc2c22cb61109
SEBASTIAN JN <sebastianjnuwu@gmail.com>
2023-02-11 23:03:34 -0300

    update src/Events/Client/MessageUpdate.js

--- src/Events/Client/MessageUpdate.js
+++ src/Events/Client/MessageUpdate.js
@@ -3,7 +3,7 @@ export default {
   name: 'messageUpdate',
   async exec(client, oldMessage, message) {
   
-    if (!message.guild || !message.author || message.author?.bot) return;
+  if (!message.guild || !message.author || message.author?.bot) return;
 
     if (message.content?.trim() !== oldMessage.content?.trim()) client.emit('messageCreate', message);
   }
