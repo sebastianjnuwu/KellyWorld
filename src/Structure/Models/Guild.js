f4405b94f056f00fa5762d6f3c8926f140e242aa
sebastianjnuwu <sebastianjnuwu@gmail.com>
2023-02-13 20:42:55 -0300

    404

--- src/Structure/Models/Guild.js
+++
@@ -1,9 +0,0 @@
-import pkg from 'mongoose';
-const { Schema, model } = pkg;
-
-export default model('Guilds', new Schema({
-  _id: { type: String },
-  antiinvite: { type: Boolean, default: false },
-  prefix: { type: String, default: '&' },
-  lang: { type: Number, default: 1 }
-}));
--- src/Structure/Models/User.js
+++
@@ -1,6 +0,0 @@
-import pkg from 'mongoose';
-const { Schema, model } = pkg;
-
-export default model('Users', new Schema({
-  _id: { type: String, default: null }
-}));
