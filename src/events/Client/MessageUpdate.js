d47f1132126baa8e56de976c5b48a59be3ba2940
sebastianjnuwu <sebastianjnuwu@gmail.com>
2023-03-04 08:29:16 -0300

    feat: add events to block invites from other servers

---
+++ src/events/Client/MessageUpdate.js
@@ -0,0 +1,10 @@
+export default {
+    name: 'messageUpdate',
+    type: 'on',
+    async exec(client, oldMessage, message) {
+        if (!message.guild || !message.author || message.author?.bot) return;
+
+        if (message.content?.trim() !== oldMessage.content?.trim())
+            client.emit('messageCreate', message);
+    },
+};
